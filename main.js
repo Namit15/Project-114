cf9a98ffb1df1304cf4fe61f28cd463b95d11664
Namit15 <92371924+Namit15@users.noreply.github.com>
2022-09-15 17:59:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+nose_x = 0;
+nose_y = 0;
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function draw ()
+{
+
+}
+
+function take_snapshot()
+{
+    
+}
